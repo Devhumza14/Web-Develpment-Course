c9f4e1b39274abff5045afdc498bf056dfea5109
Humza Naqvi <smhumzanaqvii@outlook.com>
2026-09-22 21:33:03 +0500

    today i learn about variable and object of JS

---
+++ VIdeo-55/script.js
@@ -0,0 +1,69 @@
+console.log("Hey this is JS video no 55")
+
+
+let a = 10;
+console.log(a)
+// let b = 14;
+// let c = "Humza";
+// console.log(a + b + 6)
+
+console.log(typeof a , typeof b , typeof c)
+
+{
+    let a = 17;
+    console.log(a)
+
+}
+
+
+// we should use let in modern javascript ass this when assigned outside act as a global variable and when inside any block dont act as a global variable unlike any var type var variable type also dont obey if condition usage let is recommended
+
+
+// let is blocked scoped and var is global scoped
+
+// object is key value pair
+
+let o = {
+
+    name : "Humza" ,
+    class: "BSCS" ,
+    is_handsome: true
+
+}
+
+// now to add something in object o
+
+
+o.salary = 1000;
+
+console.log(o);
+
+
+
+
+
+
+// practice questions
+
+let student_data = {
+    name: "Humza" ,
+    phone: "0301" ,
+    marks: "890"
+
+
+}
+
+console.log(student_data);
+
+// more practice
+
+let word_meaning = {
+
+    happy: "Khush",
+    sad: "Udaas",
+    ghussa:"angry",
+    pagal: "mental",
+    pretty: "pyara"
+}
+
+console.log(word_meaning);
