3d01bab7ab81702013582ace069e588bff9ba0f6
Humza Naqvi <smhumzanaqvii@outlook.com>
2026-09-24 20:05:46 +0500

    today i learn about js loops like do while while , for , for in and for of

---
+++ Video-57/index.js
@@ -0,0 +1,50 @@
+// console.log("I am loop tutorial")
+// let a  = 1
+// for( let i = 0 ; i < 100 ; i++){
+//     console.log(a + i);
+// }
+
+// for in loop use in objects iteration and seeing element and keys in objects
+
+// let obj = {
+//     name: "Humza",
+//     class: "BSCS",
+//     roll_no: "79"
+// }
+
+// obj.semester = "5th"
+
+// console.log(obj)
+
+// for (const key in obj) {
+
+//     const element = obj[key];
+//     console.log(key,element);
+
+
+// }
+
+// forof is use in array iteration 
+
+// for (const element of "Humza") {
+//     console.log(element);
+
+// }
+
+// while loop good old one
+// let i = 0;
+// while (i <= 8) {
+//     console.log(i)
+//     i++;
+
+    // }
+
+    // do while use very less used it is mostly used in some special use case where we want the loop to run one time
+    
+    let i = 10;
+    do {
+        console.log(i)
+        i++;
+
+
+    } while (i<6);
