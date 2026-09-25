75466a403a47de96955151d251073bbcd3ec0c26
Humza Naqvi <smhumzanaqvii@outlook.com>
2026-09-25 21:45:38 +0500

    i learned about functions today in JS we much fun

---
+++ Video-58/script.js
@@ -0,0 +1,46 @@
+// javascript functions
+
+// function greet(name) {
+
+//     console.log("Hi " + name + " How are you")
+    
+// }
+
+// greet("Chaudry")
+
+// function sum(a,b){
+//     // console.log(a+b)
+//     return a + b
+// }
+
+// result = sum(1,2)
+
+// console.log("the sum of number is ",  result)
+
+
+// const function1 = (x) => {
+//     console.log("helo i am arrow,", x)
+//     return x
+// }
+
+// arrow = function1(90)
+// console.log(arrow + 100)
+
+
+// function gay(bottom,upper){
+//     console.log("hello so bottom is " +bottom+ " and upper is " +upper )
+//     return bottom,upper;
+
+// }
+// gay("athar","sheri")
+
+
+const gay=(bottom,upper)=>{
+    console.log("hello so bottom is " +bottom+ " and upper is " +upper )
+    
+}
+
+gay("sheri","athar")
+
+
+
